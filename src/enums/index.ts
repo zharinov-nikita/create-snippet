f59fa867be0b5d01419c863f98a5db2aed1c4bf3
Nikita <zharinov.nicita@gmail.com>
2023-01-22 19:06:07 +0700

    feature/issue-#3 #3: closed

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,1 @@
 export * from './template-name.enum'
-export * from './prefix-name.enum'
-export * from './suffix-name.enum'
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,35 @@
+import chalk from 'chalk'
 import fs from 'fs'
 import path from 'path'
-import chalk from 'chalk'
-import { UtilStringFormatter } from './utils'
-import { enumTemplateName, enumSuffixName, enumPrefixName } from './enums'
-import { TypeConfig } from './types'
-import { config } from './config'
 import recursive from 'recursive-readdir'
+import { config } from './config'
+import { enumTemplateName } from './enums'
+import type { TypeConfig } from './types'
+import { UtilStringFormatter } from './utils'
 
 class App {
   private readonly argTemplate: string
   private readonly argName: string
-  private readonly argPrefix: string
-  private readonly argSuffix: string
   private readonly argPath: string
+  private readonly argIsPreview: string
 
   constructor() {
     this.argTemplate = this.findArg('template')
     this.argName = this.findArg('name')
-    this.argPrefix = this.findArg('prefix')
-    this.argSuffix = this.findArg('suffix')
     this.argPath = this.findArg('path')
+    this.argIsPreview = this.findArg('isPreview')
   }
 
   private findArg(name: string): string {
     const args = [...process.argv.slice(2)]
     const regexp = new RegExp(`--${name}`)
     const argName = args.find((item) => (regexp.test(item) ? item : undefined))
-    if (argName) return argName.split('=')[1]
-    throw new Error(name)
+    return argName ? argName.split('=')[1] : ''
   }
 
   private replaceAll(template: string): string {
     const name = new UtilStringFormatter(this.argName)
-    const prefix = new UtilStringFormatter(this.argPrefix)
-    const suffix = new UtilStringFormatter(this.argSuffix)
+
     return template
       .replaceAll(enumTemplateName.camelCase, `${name.toCamelCase()}`)
       .replaceAll(enumTemplateName.pascalCase, `${name.toPascalCase()}`)
@@ -41,18 +37,6 @@ class App {
       .replaceAll(enumTemplateName.upperSnakeCase, `${name.toUpperSnakeCase()}`)
       .replaceAll(enumTemplateName.lowerKebabCase, `${name.toLowerKebabCase()}`)
       .replaceAll(enumTemplateName.upperKebabCase, `${name.toUpperKebabCase()}`)
-      .replaceAll(enumPrefixName.camelCase, `${prefix.toCamelCase()}`)
-      .replaceAll(enumPrefixName.pascalCase, `${prefix.toPascalCase()}`)
-      .replaceAll(enumPrefixName.lowerSnakeCase, `${prefix.toLowerSnakeCase()}`)
-      .replaceAll(enumPrefixName.upperSnakeCase, `${prefix.toUpperSnakeCase()}`)
-      .replaceAll(enumPrefixName.lowerKebabCase, `${prefix.toLowerKebabCase()}`)
-      .replaceAll(enumPrefixName.upperKebabCase, `${prefix.toUpperKebabCase()}`)
-      .replaceAll(enumSuffixName.camelCase, `${suffix.toCamelCase()}`)
-      .replaceAll(enumSuffixName.pascalCase, `${suffix.toPascalCase()}`)
-      .replaceAll(enumSuffixName.lowerSnakeCase, `${suffix.toLowerSnakeCase()}`)
-      .replaceAll(enumSuffixName.upperSnakeCase, `${suffix.toUpperSnakeCase()}`)
-      .replaceAll(enumSuffixName.lowerKebabCase, `${suffix.toLowerKebabCase()}`)
-      .replaceAll(enumSuffixName.upperKebabCase, `${suffix.toUpperKebabCase()}`)
   }
 
   create(setting: TypeConfig): void {
@@ -68,11 +52,24 @@ class App {
           const template = JSON.stringify(fs.readFileSync(file, 'utf-8'))
           const data = this.replaceAll(template)
 
+          const fileName = path.basename(file)
           const mkdirPath = path.dirname(file).replace(path.join(config.files), path.join(this.argPath))
           const writeFilePath = path.join(file).replace(path.join(config.files), path.join(this.argPath))
 
+          if (fs.existsSync(writeFilePath)) throw new Error(this.argName)
+
+          if (this.argIsPreview === 'true') {
+            // eslint-disable-next-line no-console
+            console.log(
+              `👀 ${chalk.blue(`Preview: file ${chalk.bold(fileName)} will be created ${mkdirPath}`)}`
+            )
+            return
+          }
+
           fs.mkdirSync(mkdirPath, { recursive: true })
           fs.writeFileSync(writeFilePath, JSON.parse(data))
+          // eslint-disable-next-line no-console
+          console.log(`✅ ${chalk.green(`Success: file ${chalk.bold(fileName)} created ${mkdirPath}`)}`)
         })
       })
     } catch (e) {
