95ff30b858823359625338a0f3e95b7779dc226b
Nikita <zharinov.nicita@gmail.com>
2023-02-03 19:46:12 +0700

    update package name

--- src/utils/config/config.util.ts
+++ src/utils/config/config.util.ts
@@ -4,7 +4,7 @@ import { TypeConfig } from '../../types'
 
 export class UtilConfig {
   public generate(): TypeConfig {
-    const snippetsPath = '.code-snippet'
+    const snippetsPath = '.create-snippet'
     const snippets = fs.readdirSync(snippetsPath)
     return snippets.map((snippet) => ({ snippet, files: path.join(...[snippetsPath, snippet]) }))
   }
