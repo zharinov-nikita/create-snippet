fa9b737a01d328348ed23dcfe1b9198901061327
Nikita <zharinov.nicita@gmail.com>
2023-02-12 15:49:53 +0700

    feature/issue-#37 #37: closed

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import fs from 'fs'
 import path from 'path'
-const args = process.argv.slice(2)
+import { ModuleArgv } from '../modules'
+const moduleArgv = new ModuleArgv()
+const args = moduleArgv.getNotFormatted()
 const paths = fs.readdirSync(path.join(...['src', 'scripts']))
 const scripts = [...paths.map((script) => `--${script.split('.script')[0]}`)]
 const arg = args.filter((arg) => scripts.includes(arg))[0]
