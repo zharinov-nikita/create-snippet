299994e21f1c227dd2538011e84899fb140f6101
Nikita <zharinov.nicita@gmail.com>
2023-02-13 19:17:11 +0700

    hotfix/1.3.6 #46: closed

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { ModuleArgv } from '../modules'
 const moduleArgv = new ModuleArgv()
 const args = moduleArgv.getNotFormatted()
-const paths = fs.readdirSync(path.join(...['src', 'scripts']))
+const paths = fs.readdirSync(path.join(__dirname, '..', 'scripts'))
 const scripts = [...paths.map((script) => `--${script.split('.script')[0]}`)]
 const arg = args.filter((arg) => scripts.includes(arg))[0]
 if (typeof arg !== 'string') throw new Error('No script')
